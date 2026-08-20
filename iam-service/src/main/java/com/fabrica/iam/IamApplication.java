b076c8c1b997d6224bf09be8275efcf3a4956dc2
Jose Carlos <joseka.vf@gmail.com>
2026-08-20 19:09:58 -0300

    chore: inicializa modulo iam-service com dependencias spring e jwt

---
+++ iam-service/src/main/java/com/fabrica/iam/IamApplication.java
@@ -0,0 +1,10 @@
+package com.fabrica.iam;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IamApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(IamApplication.class, args);
+    }
+}
