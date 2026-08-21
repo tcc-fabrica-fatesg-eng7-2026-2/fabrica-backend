a0b8cb1c4f77c16118725c17245a264af7edeae4
Jose Carlos <joseka.vf@gmail.com>
2026-08-20 21:07:07 -0300

    chore: inicializa modulo equipment-service com Kafka e JPA

---
+++ equipment-service/src/main/java/com/fabrica/equipment/EquipmentApplication.java
@@ -0,0 +1,11 @@
+package com.fabrica.equipment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquipmentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EquipmentApplication.class, args);
+    }
+}
